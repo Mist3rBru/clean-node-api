a3e5a400b5b753eaeb4ee648b972728e62945476
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-25 19:16:29 -0300

    ensure it throw if no value is provided

--- src/utils/helpers/Encrypter.spec.js
+++ src/utils/helpers/Encrypter.spec.js
@@ -1,7 +1,7 @@
 const { MissingParamError } = require('../errors') 
 
 class Encrypter {
-  hash(value) { 
+  async compare(value, hashedValue) { 
     if(!value) { 
       throw new MissingParamError('value')
     }
@@ -12,3 +12,10 @@ const makeSut = () => {
   const sut = new Encrypter()
   return sut
 }
+
+describe('Encrypter', () => {
+  it('should throw if no password is provided', async () => {
+    const sut = makeSut()
+    expect(sut.compare()).rejects.toThrow(new MissingParamError('value'))
+  })
+})
