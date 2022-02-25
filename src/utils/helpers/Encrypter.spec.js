2062047de227993cbb307ab653a88d7cf201f517
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-25 19:27:07 -0300

    ensure it throw if no hash value is provided

--- src/utils/helpers/Encrypter.spec.js
+++ src/utils/helpers/Encrypter.spec.js
@@ -1,9 +1,14 @@
 const { MissingParamError } = require('../errors') 
 
+const { compare } = require('bcrypt')
+
 class Encrypter {
   async compare(value, hashedValue) { 
     if(!value) { 
       throw new MissingParamError('value')
+    }
+    if(!hashedValue) { 
+      throw new MissingParamError('hashed-value')
     }
   }
 }
@@ -14,8 +19,13 @@ const makeSut = () => {
 }
 
 describe('Encrypter', () => {
-  it('should throw if no password is provided', async () => {
+  it('should throw if no value is provided', async () => {
     const sut = makeSut()
-    expect(sut.compare()).rejects.toThrow(new MissingParamError('value'))
+    expect(sut.compare()).rejects.toThrow(new MissingParamError('value').message)
+  })
+
+  it('should throw if no hashed value is provided', async () => {
+    const sut = makeSut()
+    expect(sut.compare('any-value')).rejects.toThrow(new MissingParamError('hashed-value').message)
   })
 })
