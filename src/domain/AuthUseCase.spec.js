e7503499f9cbb30844cb0ac290ea126d386c8542
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-25 20:14:14 -0300

    ensure it throws if any param is not provided

--- src/domain/AuthUseCase.spec.js
+++ src/domain/AuthUseCase.spec.js
@@ -1,9 +1,24 @@
+const MissingParamError = require('../utils/errors/MissingParamError')
+
 class AuthUseCaseSpy { 
   async auth(email, password) {
-    
+    if(!email) { 
+      throw new MissingParamError('email')
+    }
+    if(!password) {
+      throw new MissingParamError('password')
+    }
   }
 }
 
 const makeSut = () => {
   return new AuthUseCaseSpy
 }
+
+describe('AuthUseCase', () => {
+  it('should throw if any param is not provided', () => {
+    const sut = makeSut()
+    expect(sut.auth()).rejects.toThrow(new MissingParamError('email').message)
+    expect(sut.auth('any-email')).rejects.toThrow(new MissingParamError('password').message)
+  })
+})
