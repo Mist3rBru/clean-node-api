f75a961fdf06351320fa1d04aa2aff9d49c4a5ae
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-26 17:48:18 -0300

    create mock model

--- src/infra/repositories/FindUserByEmailRepository.spec.js
+++ src/infra/repositories/FindUserByEmailRepository.spec.js
@@ -1,18 +1,37 @@
 const MissingParamError = require('../../utils/errors/MissingParamError')
 
 class FindUserByEmailRepository { 
+  constructor(model) {
+    this.model = model
+  }
+
   async find(email) { 
     if(!email) {
       throw new MissingParamError('email')
     }
+    await this.model.findOne({ email })
   }
 }
 
 const makeSut = () => {
-  const sut = new FindUserByEmailRepository()
+  const modelSpy = makeModel()
+  const sut = new FindUserByEmailRepository(modelSpy)
   return { 
-    sut
+    sut,
+    modelSpy
   }
+}
+
+const makeModel = () => {
+  class Model {
+    async findOne({ email }) { 
+      this.email = email
+      return this.user 
+    }
+  }
+  const model = new Model()
+  model.user = 'any-user'
+  return model
 }
 
 describe('FindUserByEmailRepository', () => {
