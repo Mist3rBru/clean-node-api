b166665645fc882d9e8b1a3a5ff567351354e018
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-26 14:02:02 -0300

    ensure it returns a db when valid params are provided

--- src/infra/helpers/MongoHelper.spec.js
+++ src/infra/helpers/MongoHelper.spec.js
@@ -1,25 +1,30 @@
 jest.mock('mongoose', () => ({
-  db: 'any-db',
-  
-  async createConnection(uri) {
-    this.uri = uri
-    return this.db
-  }
+	db: 'any-db',
+
+	async createConnection(uri) {
+		this.uri = uri
+		return this.db
+	},
 }))
 
 const MongoHelper = require('./MongoHelper')
 const mongoose = require('mongoose')
-const { parsed: env } = require('dotenv').config()
 
 const makeSut = () => {
-  const uri = env.MONGO_URL
-  return new MongoHelper(uri)
+	const sut = new MongoHelper('any-uri')
+	return sut
 }
 
 describe('MongoHelper', () => {
-  it('should call mongoose createConnection with correct values', async () => {
-    const sut  = new MongoHelper('any-uri')
-    await sut.connect()
-    expect(mongoose.uri).toBe('any-uri')
-  })
+	it('should call mongoose createConnection with correct values', async () => {
+		const sut = new MongoHelper('any-uri')
+		await sut.connect()
+		expect(mongoose.uri).toBe('any-uri')
+	})
+
+	it('should return db when valid uri is provided', async () => {
+		const sut = makeSut()
+		const db = await sut.connect()
+		expect(db).toBe('any-db')
+	})
 })
