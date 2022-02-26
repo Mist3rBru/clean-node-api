d5ba930bdcee8f84761d4fc694cfe947f26b9abe
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-25 23:03:29 -0300

    ensure it  returns null if invalid params are provided~

--- src/utils/helpers/TokenGenerator.spec.js
+++ src/utils/helpers/TokenGenerator.spec.js
@@ -39,7 +39,7 @@ describe('TokenGenerator', () => {
 		const sut = makeSut()
 		await sut.generate('any-payload', 'any-secret')
 		expect(jwt.payload).toBe('any-payload')
-		expect(jwt.secret).toBe(env.TOKEN_SECRET)
+		expect(jwt.secret).toBe('any-secret')
 		expect(jwt.options).toEqual({ expiresIn: '15m' })
 	})
 
@@ -47,5 +47,12 @@ describe('TokenGenerator', () => {
 		const sut = makeSut()
 		const token = await sut.generate('any-payload', 'any-secret')
 		expect(token).toBe('any-token')
+	})
+
+	it('should return null if invalid params are provided', async () => {
+		const sut = makeSut()
+		jwt.token = null
+		const token = await sut.generate('invalid-payload', 'invalid-secret')
+		expect(token).toBeNull()
 	})
 })
