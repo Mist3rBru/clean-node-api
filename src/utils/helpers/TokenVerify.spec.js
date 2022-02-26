2e748bdb63e31c67d5a7ec1552053139c00f8edc
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-26 19:08:24 -0300

    ensure it throws if no secret is provided

--- src/utils/helpers/TokenVerify.spec.js
+++ src/utils/helpers/TokenVerify.spec.js
@@ -12,10 +12,14 @@ const MissingParamError = require('../../utils/errors/MissingParamError')
 const jwt = require('jsonwebtoken')
 
 class TokenVerify {
-  async verify(token) {
+  async verify(token, secret) {
     if(!token) {
       throw new MissingParamError('token')
     }
+    if(!secret) {
+      throw new MissingParamError('secret')
+    }
+
   }
 }
 
@@ -29,5 +33,11 @@ describe('TokenVerify', () => {
     const sut = makeSut()
     const promise = sut.verify()
     expect(promise).rejects.toThrow(new MissingParamError('token'))
+  })
+
+  it('should throw if no secret is provided', async () => {
+    const sut = makeSut()
+    const promise = sut.verify('any-token')
+    expect(promise).rejects.toThrow(new MissingParamError('secret'))
   })
 })
