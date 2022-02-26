8be2bf0e81c4726101a0f23351ad2d3c3c3cb38f
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-25 21:59:45 -0300

    ensure it throws if not payload is provided

--- src/utils/helpers/TokenGenerator.spec.js
+++ src/utils/helpers/TokenGenerator.spec.js
@@ -1,10 +1,23 @@
-class TokenGenerator { 
-  async generate(payload) { 
-    return
-  }
+const MissingParamError = require('../errors/MissingParamError')
+
+class TokenGenerator {
+	async generate(payload) {
+		if (!payload) {
+			throw new MissingParamError('payload')
+		}
+		return
+	}
 }
 
 const makeSut = () => {
-  const sut = new TokenGenerator()
-  return sut
+	const sut = new TokenGenerator()
+	return sut
 }
+
+describe('TokenGenerator', () => {
+	it('should throws if no payload is provided', () => {
+		const sut = makeSut()
+		const promise = sut.generate()
+		expect(promise).rejects.toThrow(new MissingParamError('payload'))
+	})
+})
