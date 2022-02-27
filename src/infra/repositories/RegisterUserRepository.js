3244b432531ba8b284e495cd8fc7bfaa855b090b
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-27 18:43:20 -0300

    ensure it throws in no email is provided on data

--- src/infra/repositories/RegisterUserRepository.js
+++ src/infra/repositories/RegisterUserRepository.js
@@ -1,3 +1,4 @@
+const MongoHelper = require('../helpers/MongoHelper')
 const MissingParamError = require('../../utils/errors/MissingParamError')
 
 module.exports = class RegisterUserRepository {
@@ -5,5 +6,12 @@ module.exports = class RegisterUserRepository {
     if(!data) {
       throw new MissingParamError('data')
     }
+    if(!data.email){
+      throw new MissingParamError('email')
+    }
+    const userModel = await MongoHelper.getCollection('users')
+    await userModel.insertOne(data)
+    const user = await userModel.findOne({ email: data.email })
+    return user
   }
 }
--- src/infra/repositories/RegisterUserRepository.spec.js
+++ src/infra/repositories/RegisterUserRepository.spec.js
@@ -1,5 +1,8 @@
 const MissingParamError = require('../../utils/errors/MissingParamError')
 const RegisterUserRepository = require('./RegisterUserRepository')
+const MongoHelper = require('../helpers/MongoHelper')
+const env = require('../../main/config/env')
+let model 
 
 const makeSut = () => {
   const sut = new RegisterUserRepository
@@ -7,9 +10,28 @@ const makeSut = () => {
 }
 
 describe('RegisterUserRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(env.MONGO_URL)
+    model = await MongoHelper.getCollection('users')
+  })
+
+  beforeEach(async () => {
+    await model.deleteMany()
+  })
+  
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
   it('should throw if no data is provided', async () => {
     const sut = makeSut()
     const promise = sut.register()
     expect(promise).rejects.toThrow(new MissingParamError('data'))
+  })
+
+  it('should throw if no email is provided on data', async () => {
+    const sut = makeSut()
+    const promise = sut.register({})
+    expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 })
