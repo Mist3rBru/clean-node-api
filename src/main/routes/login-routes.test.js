3a88424a7655d4793bb76bca0b480d6f72bf8bbc
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-27 12:51:24 -0300

    ensure it reuturn 401 if invalid credentials are provided

--- src/main/routes/login-routes.test.js
+++ src/main/routes/login-routes.test.js
@@ -1,36 +1,45 @@
 const MongoHelper = require('../../infra/helpers/MongoHelper')
-const { hashSync } = require('bcrypt')
 const request = require('supertest')
 const app = require('../config/app')
 const env = require('../config/env')
+
+const { hashSync } = require('bcrypt')
+const hashedPassword = hashSync('any-password', 8)
+const user = {
+  name: 'any-name',
+  email: 'any-email@example.com',
+  password: 'any-password',
+  password_hash: hashedPassword,
+}
 let model
 
 describe('Login Routes', () => {
   beforeAll( async() =>{
     await MongoHelper.connect(env.MONGO_URL)
     model = await MongoHelper.getCollection('users')
-  })
-
-  beforeEach( async() => { 
-    await model.deleteMany()
+    await model.insertOne(user)
   })
 
   afterAll( async() => {
+    await model.deleteMany()
     await MongoHelper.disconnect()
   })
 
   it('should return 200 if valid credentials are provided', async () => {
-    const hash = hashSync('any-password', 8)
-    const user = {
-      name: 'any-name',
-      email: 'any-email@example.com',
-      password: 'any-password',
-      password_hash: hash,
-    }
-    await model.insertOne(user)
     await request(app)
       .post('/api/login')
       .send(user)
       .expect(200)
+  })
+
+  it('should return 401 if invalid credentials are provided', async () => {
+    const invalidCredentials = { 
+      email: 'invalid-email@example.com',
+      password: 'invalid-password'
+    }
+    await request(app)
+      .post('/api/login')
+      .send(invalidCredentials)
+      .expect(401)
   })
 })
