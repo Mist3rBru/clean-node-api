21b1d717120240d27ce871a3054a0d57a1711a99
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-27 18:18:31 -0300

    create RegisterUserRepository

---
+++ src/infra/repositories/RegisterUserRepository.spec.js
@@ -0,0 +1,12 @@
+const RegisterUserRepository = require('./RegisterUserRepository')
+
+const makeSut = () => {
+  const sut = new RegisterUserRepository
+  return sut
+}
+
+describe('RegisterUserRepository', () => {
+  it('', async () => {
+    
+  })
+})
