d211d72b3a9c408165598fa156560ed85a1d52bb
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-26 21:20:50 -0300

    remove express adapter

--- src/utils/helpers/ExpressRouterAdapter.js
+++
@@ -1,9 +0,0 @@
-module.exports = class ExpressRouterAdapter {
-	adapt(router) {
-		return async (req, res) => {
-			const httpRequest = { body: req.body }
-			const httpResponse = await router.route(httpRequest)
-			res.status(httpResponse.status).json(httpResponse.body)
-		}
-	}
-}
