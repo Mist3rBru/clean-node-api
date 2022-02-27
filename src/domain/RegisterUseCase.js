e90913aebe5a2128ea7253016a98ba68d2510dca
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-27 17:41:24 -0300

    should call encrypterGenerator with correct values

--- src/domain/RegisterUseCase.js
+++ src/domain/RegisterUseCase.js
@@ -1,9 +1,15 @@
 const { MissingParamError } = require('../utils/errors')
 
 module.exports = class RegisterUseCase {
+  constructor({ encrypterGenerator } = {}) {
+    this.encrypterGenerator = encrypterGenerator
+  }
+
   async register (body) {
     if(!body) {
       throw new MissingParamError('body')
     }
+    const { password } = body
+    this.encrypterGenerator.generate(password)
   }
 }
--- src/domain/RegisterUseCase.spec.js
+++ src/domain/RegisterUseCase.spec.js
@@ -2,10 +2,26 @@ const { MissingParamError } = require('../utils/errors')
 const RegisterUseCase = require('./RegisterUseCase')
 
 const makeSut = () => {
-  const sut = new RegisterUseCase()
+  const encrypterGeneratorSpy = makeEncrypterGenerator()
+  const sut = new RegisterUseCase({
+    encrypterGenerator: encrypterGeneratorSpy
+ })
   return {
     sut,
+    encrypterGeneratorSpy
   }
+}
+
+const makeEncrypterGenerator = () => {
+  class EncrypterGeneratorSpy {
+    async generate(password) {
+      this.password = password
+      return this.hash
+    }
+  }
+  const encrypterGeneratorSpy = new EncrypterGeneratorSpy
+  encrypterGeneratorSpy.hash = 'any-hash'
+  return encrypterGeneratorSpy
 }
 
 describe('RegisterUseCase', () => {
@@ -13,5 +29,11 @@ describe('RegisterUseCase', () => {
     const { sut } = makeSut()
     const promise = sut.register()
     expect(promise).rejects.toThrow(new MissingParamError('body'))
+  })
+
+  it('should call encrypterGenerator with correct values', async () => {
+    const { sut, encrypterGeneratorSpy } = makeSut()
+    await sut.register({ password: 'any-password'})
+    expect(encrypterGeneratorSpy.password).toBe('any-password')
   })
 })
