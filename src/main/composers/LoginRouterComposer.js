41771604c25c5b6eba03d4578871f744fa584113
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-27 12:40:31 -0300

    pass secret token by dependency injection

--- src/main/composers/LoginRouterComposer.js
+++ src/main/composers/LoginRouterComposer.js
@@ -2,10 +2,13 @@ const TokenGenerator = require('../../utils/helpers/TokenGenerator')
 const Encrypter = require('../../utils/helpers/Encrypter')
 const FindUserByEmailRepository = require('../../infra/repositories/FindUserByEmailRepository')
 const AuthUseCase = require('../../domain/AuthUseCase')
+const EmailValidator = require('../../utils/helpers/EmailValidator')
+const LoginRouter = require('../../presentation/routers/LoginRouter')
+const env = require('../config/env')
 
 module.exports = class LoginRouterCompositor {
-	static async compose() {
-		const tokenGenerator = new TokenGenerator()
+	static compose() {
+		const tokenGenerator = new TokenGenerator(env.TOKEN_SECRET)
 		const encrypter = new Encrypter()
 		const findUserByEmailRepository = new FindUserByEmailRepository()
 		const authUseCase = new AuthUseCase({
@@ -14,8 +17,6 @@ module.exports = class LoginRouterCompositor {
 			tokenGenerator,
 		})
 
-		const EmailValidator = require('../../utils/helpers/EmailValidator')
-		const LoginRouter = require('../../presentation/routers/LoginRouter')
 
 		const emailValidator = new EmailValidator()
 		return new LoginRouter({
