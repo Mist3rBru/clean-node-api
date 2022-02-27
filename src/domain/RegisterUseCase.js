641af27c55f754d2665a5ea673f2394bc4f6008d
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-27 17:24:48 -0300

    ensure it throws if no body is provided

--- src/domain/RegisterUseCase.js
+++ src/domain/RegisterUseCase.js
@@ -1,6 +1,9 @@
+const { MissingParamError } = require('../utils/errors')
 
 module.exports = class RegisterUseCase {
   async register (body) {
-    
+    if(!body) {
+      throw new MissingParamError('body')
+    }
   }
 }
--- src/domain/RegisterUseCase.spec.js
+++ src/domain/RegisterUseCase.spec.js
@@ -1,5 +1,17 @@
+const { MissingParamError } = require('../utils/errors')
+const RegisterUseCase = require('./RegisterUseCase')
+
+const makeSut = () => {
+  const sut = new RegisterUseCase()
+  return {
+    sut,
+  }
+}
+
 describe('RegisterUseCase', () => {
-  it('', async () => {
-    
+  it('should throw if no body is provided', async () => {
+    const { sut } = makeSut()
+    const promise = sut.register()
+    expect(promise).rejects.toThrow(new MissingParamError('body'))
   })
 })
