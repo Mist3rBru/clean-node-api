cf6477fe69f5001f8e3a89892bb3bbef20725a45
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-27 19:42:13 -0300

    ensure it throws if no payload is provided

--- src/utils/helpers/EncrypterGenerator.js
+++ src/utils/helpers/EncrypterGenerator.js
@@ -1,5 +1,9 @@
-module.exports = class EncrypterGenerator {
-  generate(payload) {
+const MissingParamError = require('../errors/MissingParamError')
 
+module.exports = class EncrypterGenerator {
+  async generate(payload) {
+    if(!payload) { 
+      throw new MissingParamError('payload')
+    }
   }
 }
--- src/utils/helpers/EncrypterGenerator.spec.js
+++ src/utils/helpers/EncrypterGenerator.spec.js
@@ -1,5 +1,15 @@
+const MissingParamError = require('../errors/MissingParamError')
+const EncrypterGenerator = require('./EncrypterGenerator')
+
+const makeSut = () => {
+  const sut = new EncrypterGenerator()
+  return sut
+}
+
 describe('EncrypterGenerator', () => {
-  it('', async () => {
-    
+  it('should throw if no payload is provided', async () => {
+    const sut = makeSut()
+    const promise = sut.generate()
+    expect(promise).rejects.toThrow(new MissingParamError('payload'))
   })
 })
