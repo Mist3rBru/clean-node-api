c5dd81f897c7ad41b28485f997d5b3c66ec760a5
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-27 20:59:52 -0300

    ensure server connect to database

--- src/main/server.js
+++ src/main/server.js
@@ -1,6 +1,10 @@
+const MongoHelper = require('../infra/helpers/MongoHelper')
 const app = require('./config/app')
 const env = require('./config/env')
 const port = env.APP_PORT
+
+MongoHelper.connect(env.MONGO_URL)
+	.then(console.log('MongoDB is connected'))
 
 app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`)
