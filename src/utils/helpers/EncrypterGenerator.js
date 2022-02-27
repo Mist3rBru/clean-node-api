36adb7351ef8955748389f66987071889272c7ef
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-27 19:50:08 -0300

    ensure it call bcrypt with corect values

--- src/utils/helpers/EncrypterGenerator.js
+++ src/utils/helpers/EncrypterGenerator.js
@@ -1,9 +1,11 @@
 const MissingParamError = require('../errors/MissingParamError')
+const bcrypt = require('bcrypt')
 
 module.exports = class EncrypterGenerator {
-  async generate(payload) {
-    if(!payload) { 
-      throw new MissingParamError('payload')
+  async generate(value) {
+    if(!value) { 
+      throw new MissingParamError('value')
     }
+    await bcrypt.hash(value, 8)
   }
 }
--- src/utils/helpers/EncrypterGenerator.spec.js
+++ src/utils/helpers/EncrypterGenerator.spec.js
@@ -1,5 +1,15 @@
+jest.mock('bcrypt', () => ({
+  hash: 'any-hash',
+
+  async hash (value) {
+    this.value = value
+    return this.hash
+  }
+}))
+
 const MissingParamError = require('../errors/MissingParamError')
 const EncrypterGenerator = require('./EncrypterGenerator')
+const bcrypt = require('bcrypt')
 
 const makeSut = () => {
   const sut = new EncrypterGenerator()
@@ -7,9 +17,15 @@ const makeSut = () => {
 }
 
 describe('EncrypterGenerator', () => {
-  it('should throw if no payload is provided', async () => {
+  it('should throw if no value is provided', async () => {
     const sut = makeSut()
     const promise = sut.generate()
-    expect(promise).rejects.toThrow(new MissingParamError('payload'))
+    expect(promise).rejects.toThrow(new MissingParamError('value'))
+  })
+
+  it('should call bcrypt with correct values', async () => {
+    const sut = makeSut()
+    await sut.generate('any-value')
+    expect(bcrypt.value).toBe('any-value')
   })
 })
