c420225ca4166522f3ac95bd8f4fddf6d1b4d626
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-27 11:10:01 -0300

    rename compositors to composers

--- src/main/routes/login-routes.js
+++ src/main/routes/login-routes.js
@@ -1,6 +1,6 @@
 const ExpressRouterAdapter = require('../adapters/ExpressRouterAdapter')
-const LoginRouterCompositor = require('../compositors/LoginRouterCompositor')
+const LoginRouterComposer = require('../composers/LoginRouterComposer')
 
 module.exports = (router) => {
-  router.post('/login', ExpressRouterAdapter.adapt(LoginRouterCompositor.compose()))
+  router.post('/login', ExpressRouterAdapter.adapt(LoginRouterComposer.compose()))
 }
