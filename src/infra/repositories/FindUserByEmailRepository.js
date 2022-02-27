298cb4c05e44bc76cfc7f4cda0e139daca492cc5
Bruno Rocha <brunorocha2674@gmail.com>
2022-02-27 00:30:42 -0300

    ensure it return null if no user is found

--- src/infra/repositories/FindUserByEmailRepository.js
+++ src/infra/repositories/FindUserByEmailRepository.js
@@ -1,15 +1,13 @@
 const MissingParamError = require('../../utils/errors/MissingParamError')
 const MongoHelper = require('../helpers/MongoHelper')
-const env = require('../../main/config/env')
 
 module.exports = class FindUserByEmailRepository { 
   async find(email) { 
     if(!email) {
       throw new MissingParamError('email')
     }
-    await MongoHelper.connect(env.MONGO_URL)
-    const model = await MongoHelper.getCollection('users')
-    const user = model.findOne({ 
+    const userModel = await MongoHelper.getCollection('users')
+    const user = userModel.findOne({ 
       where: { email },
       attributes: ['id', 'name', 'email']
     })
--- src/infra/repositories/FindUserByEmailRepository.spec.js
+++ src/infra/repositories/FindUserByEmailRepository.spec.js
@@ -1,13 +1,30 @@
-const MissingParamError = require('../../utils/errors/MissingParamError')
+const MongoHelper = require('../helpers/MongoHelper')
 const FindUserByEmailRepository = require('./FindUserByEmailRepository')
+const MissingParamError = require('../../utils/errors/MissingParamError')
+const env = require('../../main/config/env')
+let model
 
 const makeSut = () => {
-  const sut = new FindUserByEmailRepository()
-  return sut
+  return new FindUserByEmailRepository()
 }
 
 describe('FindUserByEmailRepository', () => {
-  it('', async () => {
-    expect(null).toBe(null)
+  beforeAll( async() =>{
+    await MongoHelper.connect(env.MONGO_URL)
+    model = await MongoHelper.getCollection('users')
+  })
+
+  beforeEach( async() => { 
+    await model.deleteMany()
+  })
+
+  afterAll( async() => {
+    await MongoHelper.disconnect()
+  })
+
+  it('should return null if no user is found', async () => {
+    const sut = makeSut()
+    const user = await sut.find('any-email')
+    expect(user).toBeNull()
   })
 })
